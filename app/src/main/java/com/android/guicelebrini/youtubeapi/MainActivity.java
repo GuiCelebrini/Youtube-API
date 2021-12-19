8db615a7cc70e49466952dcf393fbb044c17e9d9
GuiCelebrini <celebrinig@gmail.com>
2021-12-19 14:45:26 -0300

    Adding StateChangeListener for YoutubePlayer

--- app/src/main/java/com/android/guicelebrini/youtubeapi/MainActivity.java
+++ app/src/main/java/com/android/guicelebrini/youtubeapi/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends YouTubeBaseActivity {
     private YouTubePlayerView playerView;
 
     private YouTubePlayer.PlaybackEventListener eventListener;
+    private YouTubePlayer.PlayerStateChangeListener stateChangeListener;
 
     public static final String GOOGLE_API_KEY = "AIzaSyBIkJyyaMXwVcAnr52DM5nAXCOOqh2eFKE";
 
@@ -24,6 +25,36 @@ public class MainActivity extends YouTubeBaseActivity {
         setContentView(R.layout.activity_main);
         findViewsById();
 
+        createPlaybackEventListener();
+
+        createStateChangeListener();
+
+        playerView.initialize(GOOGLE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasPlaying) {
+                Toast.makeText(getApplicationContext(), "Player initialized successfully", Toast.LENGTH_SHORT).show();
+                youTubePlayer.setPlaybackEventListener(eventListener);
+                youTubePlayer.setPlayerStateChangeListener(stateChangeListener);
+
+
+                if (!wasPlaying) {
+                    youTubePlayer.cuePlaylist("PL15FF6F444D5A4B8E");
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Toast.makeText(getApplicationContext(), "Player initialization failed - " + youTubeInitializationResult.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void findViewsById() {
+        playerView = findViewById(R.id.viewYoutubePlayer);
+    }
+
+    private void createPlaybackEventListener(){
         eventListener = new YouTubePlayer.PlaybackEventListener() {
             @Override
             public void onPlaying() {
@@ -42,7 +73,7 @@ public class MainActivity extends YouTubeBaseActivity {
 
             @Override
             public void onBuffering(boolean b) {
-
+                //loading video
             }
 
             @Override
@@ -50,28 +81,38 @@ public class MainActivity extends YouTubeBaseActivity {
 
             }
         };
-
-        playerView.initialize(GOOGLE_API_KEY, new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasPlaying) {
-                Toast.makeText(getApplicationContext(), "Player initialized successfully", Toast.LENGTH_SHORT).show();
-                youTubePlayer.setPlaybackEventListener(eventListener);
-
-
-                if (!wasPlaying) {
-                    youTubePlayer.loadPlaylist("PL15FF6F444D5A4B8E");
-                }
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                Toast.makeText(getApplicationContext(), "Player initialization failed - " + youTubeInitializationResult.toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
     }
 
-    private void findViewsById() {
-        playerView = findViewById(R.id.viewYoutubePlayer);
+    private void createStateChangeListener(){
+        stateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+            @Override
+            public void onLoading() {
+            }
+
+            @Override
+            public void onLoaded(String s) {
+
+            }
+
+            @Override
+            public void onAdStarted() {
+
+            }
+
+            @Override
+            public void onVideoStarted() {
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+        };
     }
 }
